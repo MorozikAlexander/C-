d85e357b38bf8450a737b872f4ad1e181f50ac2c
Morozik Alexander <morozik.alexander@gmail.com>
2015-04-17 02:33:44 +0300

    1

---
+++ Test00-01/LinearFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test00_01
+{
+    public class LinearFunction
+    {
+        private double a = 0;
+        private double b = 0;
+
+        public double ValueA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double ValueB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double y(double x)
+        {
+            return a * x + b;
+        }
+    }
+}
--- Test00-01/Program.cs
+++ Test00-01/Program.cs
@@ -10,6 +10,32 @@ namespace Test00_01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("         Программа вычисления значения функции Y = A*X + B");
+            ConsoleKeyInfo cki;
+            LinearFunction myfunction = new LinearFunction();
+            do
+            {
+                Console.WriteLine("ESC - выход; A, B - изменение параметров; X - возврат значения функции ");
+                Console.WriteLine("A = {0}; B= {1}", myfunction.ValueA, myfunction.ValueB);
+                cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Write("Введите значение А:");
+                        myfunction.ValueA = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case ConsoleKey.B:
+                        Console.Write("Введите значение B:");
+                        myfunction.ValueB = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case ConsoleKey.X:
+                        Console.Write("Введите значение X:");
+                        Console.WriteLine("Получено значение Y:" + myfunction.y(Convert.ToDouble(Console.ReadLine())));
+                        break;
+                    default:
+                        break;
+                }
+            } while (cki.Key != ConsoleKey.Escape);
         }
     }
 }
