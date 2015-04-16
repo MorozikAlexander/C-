044fab146e42e3a6183b61ec01b3b2bff08678a6
Morozik Alexander <morozik.alexander@gmail.com>
2015-04-17 02:45:14 +0300

    1

---
+++ Test00-02/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test00_02
+{
+    class Item
+    {
+        private string name;
+        private int cost;
+        private int count;
+
+        public string NameValue
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int CostValue
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        public int CountValue
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        public int GeneralCostValue()
+        {
+            return cost * count;
+        }
+    }
+}
