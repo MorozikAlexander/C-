d9019c42e983bea8c44d836328d27ab4dffaa30c
Morozik Alexander <morozik.alexander@gmail.com>
2015-04-20 15:17:34 +0300

    1

---
+++ Test00-03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test00_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         Программа работы с треугольниками:");
+            ConsoleKeyInfo cki;
+            Triangle mt = new Triangle();
+            do
+            {
+                Console.WriteLine("ESC - выход; A, B, C - изменение сторон; X - возврат значения функции ");
+                Console.WriteLine("A = {0}; B = {1}; С = {2}; Тип = {3}.", mt.A, mt.B, mt.C, mt.PrintTriangleType());
+                cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.Write("Введите значение А:");
+                        mt.A = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case ConsoleKey.B:
+                        Console.Write("Введите значение B:");
+                        mt.B = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case ConsoleKey.C:
+                        Console.Write("Введите значение C:");
+                        mt.C = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        break;
+                }
+                mt.CheckTriangleType();
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Test00-03/Triangle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test00_03
+{
+    class Triangle
+    {
+        private int[] sides = new int[3] {0, 0, 0};
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        private enum TriangleType {Rect, Obtus, Acute, Unknown};
+        private TriangleType mytriangletype;        
+
+        public Triangle()
+        {
+            mytriangletype = TriangleType.Unknown;
+            A = 0;
+            B = 0;
+            C = 0;            
+        }
+
+        public void CheckTriangleType()
+        {
+            mytriangletype = GetTriangleType(A, B, C);
+        }
+
+        private bool isValidTriangle(int x, int y, int z)
+        {
+            if ((x<=0)||(y<=0)||(z<=0))
+            {
+                return false;
+            } 
+            else
+            {
+                sides[0] = x;
+                sides[1] = y;
+                sides[2] = z;
+                int m = 0;
+                for (int i = 2; i > 0; i--)
+                {
+                    if (sides[i] > sides[i - 1])
+                    {
+                        m = sides[i];
+                        sides[i] = sides[i-1];
+                        sides[i - 1] = m;
+                    }                    
+                }
+                if (sides[0] < (sides[1] + sides[2]))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }                
+            }
+        }
+
+        private TriangleType GetTriangleType(int xx, int yy, int zz)
+        {
+            if (isValidTriangle(xx, yy, zz) == true)
+            {
+                if (sides[0] * sides[0] == sides[1] * sides[1] + sides[1] * sides[1])
+                {
+                    return TriangleType.Rect;
+                }
+                else if (sides[0] * sides[0] > sides[1] * sides[1] + sides[1] * sides[1])
+                {
+                    return TriangleType.Obtus;
+                }
+                else if (sides[0] * sides[0] < sides[1] * sides[1] + sides[1] * sides[1])
+                {
+                    return TriangleType.Acute;
+                }
+                else
+                {
+                    return TriangleType.Unknown;
+                }
+            }
+            else
+            {
+                return TriangleType.Unknown;
+            }
+        }
+
+        public string PrintTriangleType()
+        {
+            switch (mytriangletype)
+            {
+                case TriangleType.Rect:
+                    return "Прямоугольный";
+                    break;
+                case TriangleType.Obtus:
+                    return "Тупоугольный";
+                    break;
+                case TriangleType.Acute:
+                    return "Остроугольный";
+                    break;
+                case TriangleType.Unknown:
+                    return "Неопределенный";
+                    break;            
+                default:
+                    return "";
+                    break;
+            }
+        }
+
+    }
+}
